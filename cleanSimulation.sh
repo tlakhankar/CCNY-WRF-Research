5e661dfdf63eab3b842046dd17dbe47ff9973901
Kendall Molas <24480278+WK-M@users.noreply.github.com>
2019-07-15 13:46:08 -0400

    Create cleanSimulation.sh

---
+++ cleanSimulation.sh
@@ -0,0 +1,2 @@
+## Located in SIMULATION directory, used to clean directory to get rid of external files
+rm *.LDASIN_DOMAIN1 *.LDASIN_DOMAIN1.nc GFS2WRFHydro_weight_bilinear.nc  SCRIP_WRFHydro_bilinear.nc  PET0.RegridWeightGen.Log SCRIP_GFS_bilinear.nc 
