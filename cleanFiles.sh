6a1cd5c407c6904ed957125e13d510c40c355bd3
Kendall Molas <24480278+WK-M@users.noreply.github.com>
2019-07-15 13:45:25 -0400

    Create cleanFiles.sh

---
+++ cleanFiles.sh
@@ -0,0 +1,4 @@
+## Located in regrid directory.
+## Cleans files in directory, to allow regrid to be ran again.
+
+rm *.LDASIN_DOMAIN1 *.LDASIN_DOMAIN1.nc GFS2WRFHydro_weight_bilinear.nc  SCRIP_WRFHydro_bilinear.nc  PET0.RegridWeightGen.Log SCRIP_GFS_bilinear.nc 
