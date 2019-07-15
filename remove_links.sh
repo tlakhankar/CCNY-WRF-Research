b86a879612e971a229df791689a787459f850bae
Kendall Molas <24480278+WK-M@users.noreply.github.com>
2019-07-15 13:44:21 -0400

    Update remove_links.sh

--- remove_links.sh
+++ remove_links.sh
@@ -1,13 +1,14 @@
 ### This script allows the ability to remove symbolic links if any errors were experienced when regridding
-
 #!/bin/bash
 
 if [ "$#" -ne 3 ]; then
-	echo "3 parameters must exist"
-	echo "USAGE: ./remove_links.sh [START] [END] [PRESSURE LEVEL]"
+echo "3 parameters must exist"
+echo "USAGE: ./remove_links.sh [START] [END] [PRESSURE LEVEL]"
+exit 1
 fi
 
 for i in $(seq -f "%03g" $1 $2)
 do 
-	rm "/home/docker/Jean\ Files/input_files/gfs.t00z.pgrb2.$3.f$i"
+	file="gfs.t00z.pgrb2.$3.f$i"
+	rm "/home/docker/Jean Data/regrid/input_files/$file"
 done
