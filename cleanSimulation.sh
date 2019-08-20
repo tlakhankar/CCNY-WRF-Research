dd21fc42e7e7efde318b274da846475b762c291c
Kendall <kmolas@nyit.edu>
2019-08-19 20:34:35 -0400

    Removing unnecessary scripts

--- cleanSimulation.sh
+++
@@ -1,2 +0,0 @@
-## Located in SIMULATION directory, used to clean directory to get rid of external files
-rm *CHRTOUT_DOMAIN1 *.CHANOBS_DOMAIN1 *.LDASOUT_DOMAIN1 *.RTOUT_DOMAIN1 frxst_pts_out.txt  diag_hydro.00001 diag_hydro.00000 RESTART.*_DOMAIN1
