d53ae0d6401e7d8d70bb6bd92a6d686688e900cb
AbtahiChowdhury <abtahichowdhury@gmail.com>
2020-10-25 15:16:07 -0400

    did plot of watershed e and f

--- Plotting_GOES-R/dischargeInput/discharge.py
+++ Plotting_GOES-R/dischargeInput/discharge.py
@@ -3,16 +3,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def lockandload(fileName):
-    #LOAD CSV
-    filename = "frxst_pts_out_1p00.csv"
-    df = pd.read_csv(filename)
-    data = df.values
-    return data
+def plot(filename):
 
+    dp = lambda x: pd.datetime.strptime(x,'%Y-%m-%d %H:%M')
+    df = pd.read_csv(
+        filename,
+        sep='\t',
+        parse_dates=['datetime'],
+        date_parser=dp
+    )
+    df = df.dropna()
 
-def plot(data):
-    #PLOT THE STUFF
+    print(df[['datetime','123955_00065']])
+    ax = df.plot(x='datetime', y='123955_00065', kind='line',title='USGS 50043800 Rio de La Plata at Comerio, PR')
+    ax.set(xlabel='Date', ylabel='Discharge (cubic feet per second)')
+    ax.legend(['Site No. 50043800'])
+    plt.tight_layout()
+    plt.show()
 
-
-stuff = lockandload()
+if __name__ == "__main__":
+    plot('nwis.waterdata.usgs.govF.tsv')
