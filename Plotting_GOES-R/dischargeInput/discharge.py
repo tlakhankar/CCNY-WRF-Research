88c9750b0c54a1a6ec6653fd25d36ebd7e96056b
AbtahiChowdhury <abtahichowdhury@gmail.com>
2020-11-01 13:38:07 -0500

    tried to plot percipitation

--- Plotting_GOES-R/dischargeInput/discharge.py
+++ Plotting_GOES-R/dischargeInput/discharge.py
@@ -12,14 +12,16 @@ def plot(filename):
         parse_dates=['datetime'],
         date_parser=dp
     )
-    df = df.dropna()
+    df = df.fillna(0)
 
-    print(df[['datetime','123955_00065']])
-    ax = df.plot(x='datetime', y='123955_00065', kind='line',title='USGS 50043800 Rio de La Plata at Comerio, PR')
+    print(df.head())
+    #print(df[['datetime','123955_00065','123956_00060']])
+    ax = df.plot(x='datetime', y='123892_00060', kind='line',title='USGS 50043800 Rio de La Plata at Comerio, PR')
+    df.plot(ax=ax, x='datetime', y='233641_00045', kind='line')
     ax.set(xlabel='Date', ylabel='Discharge (cubic feet per second)')
-    ax.legend(['Site No. 50043800'])
+    ax.legend(['Discharge','Precipitation'])
     plt.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
-    plot('nwis.waterdata.usgs.govF.tsv')
+    plot('nwis.waterdata.usgs.govE.tsv')
