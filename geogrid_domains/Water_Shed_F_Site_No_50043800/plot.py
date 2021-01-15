50313b75432d6c3ce20a328ea1566bc73ca3de38
aidrisy0139 <arik.idrisy@gmail.com>
2021-01-15 15:51:48 -0500

    Copy plot.py for Water_Shed_F

---
+++ geogrid_domains/Water_Shed_F_Site_No_50043800/plot.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dateparse = lambda x: pd.datetime.strptime(x,'%m/%d/%Y %H:%M')
+df = pd.read_csv("nwis.waterdata.usgs.gov.tsv", sep="\t", usecols=['Date', 'Discharge'],date_parser=dateparse)
+#df = pd.read_csv('actual_clean2.csv',names=['Date','Discharge'],sep=',',parse_dates=['Date'],date_parser=dateparse)
+
+print(df)
+#yData = df["123892_00060"].values
+#xData = df["datetime"].values
+
+ax = df.plot(x='Date',y='Discharge',kind='line',title="USGS Discharge Rates for Watershed F")
+
+
+ax.set(xlabel='Date',ylabel='Discharge Rate (ft^3/s)')
+plt.show()
+'''
+print(xData.size)
+print(yData.size)
+plt.plot(yData)
+'''
+'''
+x = np.arange(0,3*np.pi,0.1)
+y = np.sin(x)
+plt.plot(x,y)
+'''
+plt.show()
+
+
